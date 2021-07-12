310e60b395f874731a675570d644962a6d7a3a07
Almir Campos <>
2021-07-12 03:11:37 -0700

    [WIP][U39] Quick backup

--- react-state-carousel-coins/carousel/src/Carousel.smoke.test.js
+++ react-state-carousel-coins/carousel/src/Carousel.smoke.test.js
@@ -2,6 +2,6 @@ import React from "react";
 import { render } from "@testing-library/react";
 import Carousel from "./Carousel";
 
-it("renders without crashing", function() {
+it("renders Carousel without crashing", function() {
   render(<Carousel />);
 });
